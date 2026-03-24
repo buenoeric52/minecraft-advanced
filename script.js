796b6438ba50e510e69833e9d47f347dd7925c93
꧁༺ERICK༻꧂ <buenoeric52@gmail.com>
2026-03-24 17:34:05 -0400

    Minecraft-advanced

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ document.querySelectorAll('.btn').forEach(boton => {
         console.log("Botón presionado: " + boton.innerText);
     });
 });
+// Lógica para quitar la animación de inicio
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        const loader = document.getElementById('loader');
+        if (loader) {
+            loader.style.opacity = '0';
+            setTimeout(() => {
+                loader.style.display = 'none';
+            }, 800);
+        }
+    }, 3000); // 3000 significa 3 segundos
+});
+// Añade esto si creas el botón de comentarios:
+function enviarComentario() {
+    const box = document.querySelector('.comment-box textarea');
+    if(box.value.trim() !== "") {
+        alert("¡Erick recibió tu mensaje! Gracias por el feedback.");
+        box.value = ""; // Esto limpia el cuadro después de enviar
+    } else {
+        alert("¡Escribe algo primero!");
+    }
+}
